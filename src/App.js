c2bbf9345a4c8006397db63a65ac840170c09e4d
habeebullah-web <habeebu994@gmail.com>
2023-07-12 12:48:12 +0500

    change name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MY NAME IS HABEEEEEEEEEBULLAH
+          MY NAME IS ALI HAIDER
         </p>
         <a
           className="App-link"
